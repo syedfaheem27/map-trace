780361ac368ad64d3ff017520a17f98ca466863d
syedfaheem27 <syedfaheem2719@gmail.com>
2023-08-14 19:24:14 +0530

    Refactored the code and add all the logic in the App class

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # map-trace
 
-An application that traces out all your physical activities on a map.
+# About
 
-It uses the browsers geolocation api to fetch the location of the user.
+An application that traces out all your physical activities on a map
+and puts them on a list for you to see. You can add an activity by
+clicking on any location on a map and describe the activity and it's details
+and submit it which then adds it to the list of workouts that are being rendered
+on the side.
+
+It uses the browsers geolocation api to fetch the location of the user and uses
+the third party leaflet library to render the map.
+
+# Architecture
+
+The architecture that's being employed for this application is based
+on classes and objects where private and public fields and methods
+have been used.
--- script.js
+++ script.js
@@ -11,80 +11,85 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-//variables storing datat about the event
-let map, mapEvent;
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude, longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+class App {
+  #map;
+  #mapEvent;
 
-      const coords = [latitude, longitude];
+  constructor() {
+    this._getLocation();
 
-      map = L.map("map").setView(coords, 13);
+    //Handling the change event on the form
+    inputType.addEventListener("change", this._toggleActivity);
 
-      L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+    //Submit event handling
+    form.addEventListener("submit", this._submitForm.bind(this));
+  }
 
-      map.on("click", function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove("hidden");
-        inputDistance.focus();
-
-        //   const { lat, lng } = mapEvent.latlng;
-        //   L.marker([lat, lng])
-        //     .addTo(map)
-        //     .bindPopup(
-        //       L.popup([lat, lng], {
-        //         maxWidth: 250,
-        //         minWidth: 100,
-        //         autoClose: false,
-        //         closeOnClick: false,
-        //         className: "running-popup",
-        //       }).setContent("Cycling")
-        //     )
-        //     .openPopup();
-      });
-    },
-    function () {
-      alert("Failed to get the co-ordinates");
+  _getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("Failed to get the co-ordinates");
+        }
+      );
     }
-  );
+  }
+
+  _loadMap(position) {
+    const { latitude, longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+
+    this.#map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on("click", this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove("hidden");
+    inputDistance.focus();
+  }
+
+  _toggleActivity() {
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+
+  _submitForm(e) {
+    e.preventDefault();
+
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup([lat, lng], {
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: `${inputType.value}-popup`,
+        }).setContent(inputType.value)
+      )
+      .openPopup();
+
+    //Clear input fields
+    inputCadence.value =
+      inputDistance.value =
+      inputDuration.value =
+      inputElevation.value =
+        "";
+    inputType.value = "running";
+
+    form.classList.add("hidden");
+  }
 }
 
-//Submit event handling
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup([lat, lng], {
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: `${inputType.value}-popup`,
-      }).setContent(inputType.value)
-    )
-    .openPopup();
-
-  //Clear input fields
-  inputCadence.value =
-    inputDistance.value =
-    inputDuration.value =
-    inputElevation.value =
-      "";
-  inputType.value = "running";
-
-  form.classList.add("hidden");
-});
-
-//Handling the change event on the form
-inputType.addEventListener("change", function () {
-  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-});
+const app = new App();
